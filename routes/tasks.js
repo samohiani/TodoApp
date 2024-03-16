d95400153c3ee8794adf2b538be21f8e0e0c1b52
samohiani <ohianisammy2005@gmail.com>
2024-03-16 11:09:40 +0100

    change to get the tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,7 +5,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const tasks = await Task.find().select("name");
+  const tasks = await Task.find().select("-_id name");
   res.send(tasks);
 });
 
